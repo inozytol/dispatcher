d0a218a6f4a19da3bcecdd518771658e2fa83ce6
gN <inozytol@gmail.com>
2019-02-09 23:12:40 +0100

    Added two argument git file to interface

--- src/main/inozytol/fileDispatcher/FileFetcherDispatcherById.java
+++ src/main/inozytol/fileDispatcher/FileFetcherDispatcherById.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 
 public interface FileFetcherDispatcherById {
     Path getFile(String id);
+    Path getFile(String id, Path targetPath);
     String storeFile(Path fileToStore);
     boolean removeFile(String id);
     String [] fileList();
